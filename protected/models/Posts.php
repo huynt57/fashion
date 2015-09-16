d32a40b0d35f48fd20385465f1a4aa57577a3c8d
huynt <huynt57@gmail.com>
2015-09-16 19:48:23 +0700

    fix smt

--- protected/models/Posts.php
+++ protected/models/Posts.php
@@ -2,44 +2,40 @@
 
 Yii::import('application.models._base.BasePosts');
 
-class Posts extends BasePosts
-{
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
-        
-        public function addPost($user_id, $post_content, $location, $url_arr)
-        {
-            $model = new Posts;
-            $model->post_content = $post_content;
-            $model->post_comment_count = 0;
-            $model->post_like_count = 0;
-            $model->location = $location;
-            $model->created_at = time();
-            $model->status = 1;
-            $model->updated_at = time();
-            $model->user_id = $user_id;
-            if(!$model->save(FALSE))
-            {
+class Posts extends BasePosts {
+
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    public function addPost($user_id, $post_content, $location, $url_arr) {
+        $model = new Posts;
+        $model->post_content = $post_content;
+        $model->post_comment_count = 0;
+        $model->post_like_count = 0;
+        $model->location = $location;
+        $model->created_at = time();
+        $model->status = 1;
+        $model->updated_at = time();
+        $model->user_id = $user_id;
+        if (!$model->save(FALSE)) {
+            return FALSE;
+        }
+        foreach ($url_arr as $url) {
+            $image = new Images;
+            $image->img_url = $url;
+            $image->post_id = $model->post_id;
+            $image->created_at = time();
+            $image->created_by = $user_id;
+            $image->status = 1;
+            if (!$image->save(FALSE)) {
                 return FALSE;
             }
-            foreach ($url_arr as $url) 
-            {
-                $image = new Images;
-                $image->img_url = $url;
-                $image->post_id = $model->post_id;
-                $image->created_at = time();
-                $image->created_by = $user_id;
-                $image->status = 1;
-                if(!$image->save(FALSE))
-                {
-                    return FALSE;
-                }
-            }
-            return TRUE;
         }
-        
-        public function getPostByUser($user_id, $limit, $offset) {
+        return TRUE;
+    }
+
+    public function getPostByUser($user_id, $limit, $offset) {
         $data = Yii::app()->db->createCommand()
                 ->select('*')
                 ->from('tbl_posts p')
@@ -50,4 +46,5 @@ class Posts extends BasePosts
                 ->queryAll();
         return $data;
     }
+
 }
