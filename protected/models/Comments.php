1f14c762a2328cd228660485a06f06c1b4d491eb
huynt <huynt57@gmail.com>
2015-11-27 09:18:24 +0700

    update comment

--- protected/models/Comments.php
+++ protected/models/Comments.php
@@ -44,7 +44,7 @@ class Comments extends BaseComments {
 //                ->offset($offset)
 //                ->order('comment_id DESC')
 //                ->queryAll();
-        $sql = "SELECT tbl_comments.*, tbl_user.username FROM tbl_comments JOIN tbl_user ON tbl_comments.created_by = tbl_user.id WHERE tbl_comments.post_id = $post_id ORDER BY tbl_comments.comment_id DESC LIMIT $offset, $limit ";
+        $sql = "SELECT tbl_comments.*, tbl_user.username, tbl_user.photo FROM tbl_comments JOIN tbl_user ON tbl_comments.created_by = tbl_user.id WHERE tbl_comments.post_id = $post_id ORDER BY tbl_comments.comment_id DESC LIMIT $offset, $limit ";
         $data = Yii::app()->db->createCommand($sql)->queryAll();
         return $data;
     }
